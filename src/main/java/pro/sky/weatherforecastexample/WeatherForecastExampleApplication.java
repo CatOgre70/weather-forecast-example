05aca83408bbe0cbecdd6a3d44018fe092a41dba
Vasily Demin <vasily.demin@gmail.com>
2022-11-08 12:05:12 +0300

    Initial commit

---
+++ src/main/java/pro/sky/weatherforecastexample/WeatherForecastExampleApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.weatherforecastexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastExampleApplication.class, args);
+    }
+
+}
